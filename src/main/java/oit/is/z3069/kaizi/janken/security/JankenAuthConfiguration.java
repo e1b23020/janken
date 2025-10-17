9269c8584c9f3d79b07fe73597916f0fc3e8e565
e1b23020 <e1b23020@oit.ac.jp>
2025-10-17 14:56:30 +0900

    セキュリティの認証処理を実装

---
+++ src/main/java/oit/is/z3069/kaizi/janken/security/JankenAuthConfiguration.java
@@ -0,0 +1,41 @@
+package oit.is.z3069.kaizi.janken.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class JankenAuthConfiguration {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.formLogin(login -> login
+        .permitAll())
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/")) // ログアウト後に / にリダイレクト
+        .authorizeHttpRequests(authz -> authz
+            .requestMatchers("/janken/**").authenticated() // jankenにアクセスしたときだけ認識
+            .anyRequest().permitAll()) // 上記以外は全員アクセス可能
+        .csrf(csrf -> csrf
+            .ignoringRequestMatchers("/sample2*/**")); // sample2用にCSRF対策を無効化
+    return http.build();
+  }
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+
+    UserDetails user1 = User.withUsername("user1")
+        .password("{bcrypt}$2y$05$zCNmf7.ZJTzGIgh1HOZ9zuTsdnXdQbw3imuyp7zr/mTTtxSGelpba").roles("USER").build();
+    UserDetails user2 = User.withUsername("user2")
+        .password("{bcrypt}$2y$05$Q5CYogMnav6uujotXj6An.edIPDeYvLgeXYcFIt0N4tXBPpxNfJ0i").roles("USER").build();
+    return new InMemoryUserDetailsManager(user1, user2);
+  }
+
+}
